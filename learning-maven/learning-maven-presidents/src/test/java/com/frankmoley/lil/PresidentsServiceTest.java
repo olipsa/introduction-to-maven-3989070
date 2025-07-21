11200a78251bb1d7bd218fdee07141dd5599956e
Ondina-Patricia Lipșa <58936202+olipsa@users.noreply.github.com>
2025-07-21 17:55:24 +0000

    Added JavaDoc reporting

--- learning-maven/learning-maven-presidents/src/test/java/com/frankmoley/lil/PresidentsServiceTest.java
+++ learning-maven/learning-maven-presidents/src/test/java/com/frankmoley/lil/PresidentsServiceTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PresidentsServiceTest {
+public class PresidentsServiceTest {
 
     @Test
     void getPresidents() {
